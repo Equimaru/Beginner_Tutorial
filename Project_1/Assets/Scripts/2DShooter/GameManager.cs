41dde5cff9bdbbdd71c3556ce7569953379321af
Equimaru <119777912+Equimaru@users.noreply.github.com>
2024-04-22 21:02:12 +0300

    HW 3/4

--- Project_1/Assets/Scripts/2DShooter/GameManager.cs
+++ Project_1/Assets/Scripts/2DShooter/GameManager.cs
@@ -12,20 +12,41 @@ public class GameManager : MonoBehaviour
 
     public GameObject winTitle;
     public GameObject target;
-    
+
+    #region Audio
+    private AudioSource _audioSource;
+    [SerializeField] private AudioClip hitSound;
+    [SerializeField] private AudioClip missSound;
+    [SerializeField] private AudioClip reload;
+    #endregion
+
+    #region Ammo
+    [SerializeField] private GameObject cartrige_1;
+    [SerializeField] private GameObject cartrige_2;
+    [SerializeField] private GameObject cartrige_3;
+    [SerializeField] private GameObject cartrige_4;
+    [SerializeField] private GameObject cartrige_5;
+    #endregion
+
     [SerializeField] private TextMeshProUGUI scoreText;
     private Camera _cam;
 
     private InputActions _gameInput;
-    
+
+    private int ammoInMag;
     private int _score = 0;
     private bool _win = false;
     public LayerMask droneLayer;
-    
+
     private void Start()
     {
+        ammoInMag = 5;
+        AmmunitionInitialization();
+
         _gameInput = new InputActions();
         _gameInput?.Player.Enable();
+
+        _audioSource = GetComponent<AudioSource>();
 
         _cam = Camera.main;
         if (_cam != null)
@@ -43,11 +64,6 @@ public class GameManager : MonoBehaviour
         if (_win == true)
         {
             CancelInvoke(nameof(Spawn));
-        }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            GetComponent<AudioSource>().Play();
         }
     }
 
@@ -69,20 +85,79 @@ public class GameManager : MonoBehaviour
         scoreText.text = _score.ToString();
 
         if (_score < 10) return;
-        
+
         _win = true;
         winTitle.SetActive(true);
     }
 
     private void CheckForHit()
     {
+        if (ammoInMag == 0) return;
+
         RaycastHit2D hit = Physics2D.Raycast(_cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)), Vector2.zero, droneLayer);
-        
-        if(hit.collider != null)
+
+        if (hit.collider != null)
         {
             IncrementScore();
+            _audioSource.clip = hitSound;
+            _audioSource.Play();
             GameObject drone = hit.collider.gameObject;
             Destroy(drone);
         }
+        else
+        {
+            _audioSource.clip = missSound;
+            _audioSource.Play();
+        }
+
+        AmmunitionManagement();
+    }
+
+    private void AmmunitionInitialization()
+    {
+        cartrige_1.SetActive(true);
+        cartrige_2.SetActive(true);
+        cartrige_3.SetActive(true);
+        cartrige_4.SetActive(true);
+        cartrige_5.SetActive(true);
+    }
+
+
+    private void AmmunitionManagement()
+    {
+        switch (ammoInMag)
+        {
+            case 1:
+                cartrige_1.SetActive(false);
+                ammoInMag = 0;
+                StartCoroutine(waitForReload());
+                break;
+            case 2:
+                cartrige_2.SetActive(false);
+                ammoInMag = 1;
+                break;
+            case 3:
+                cartrige_3.SetActive(false);
+                ammoInMag = 2;
+                break;
+            case 4:
+                cartrige_4.SetActive(false);
+                ammoInMag = 3;
+                break;
+            case 5:
+                cartrige_5.SetActive(false);
+                ammoInMag = 4;
+                break;
+        }
+    }
+
+    private IEnumerator waitForReload()
+    {
+        _audioSource.clip = reload;
+        _audioSource.Play();
+        yield return new WaitForSeconds(1);
+        AmmunitionInitialization();
+        yield return new WaitForSeconds(1);
+        ammoInMag = 5;
     }
 }
