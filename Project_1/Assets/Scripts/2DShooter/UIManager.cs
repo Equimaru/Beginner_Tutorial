e21a035e20a7d8cd44ac1b177ac031e7c038999b
Equimaru <119777912+Equimaru@users.noreply.github.com>
2024-05-09 20:48:07 +0300

    1/2 2DShooter UI

--- Project_1/Assets/Scripts/2DShooter/UIManager.cs
+++ Project_1/Assets/Scripts/2DShooter/UIManager.cs
@@ -1,8 +1,32 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject winTitle;
+    [SerializeField] private GameObject winTitle;
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private GameObject MusicButton;
+    [SerializeField] private GameObject sFXButton;
+    [SerializeField] private Button settingsButton;
+    [SerializeField] private Button resetButton;
+    [SerializeField] private Button exitButton;
+
+    private bool settingsPanelIsOpened;
+
+    public void ToggleSettingsPanel()
+    {
+        if (settingsPanelIsOpened)
+        {
+
+        }
+    }
+
+    private void OpenSettingsPanel()
+    {
+
+    }
+
+
 
     public void ShowWinTitle()
     {
