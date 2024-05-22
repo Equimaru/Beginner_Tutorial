7aa3acc19218b21e4b524178613e6beb5c47805a
Equimaru <119777912+Equimaru@users.noreply.github.com>
2024-05-22 19:06:18 +0300

    Added rotation to food

--- Project_2/Assets/CatchGameAssets/Scripts/ObjectToCatch.cs
+++ Project_2/Assets/CatchGameAssets/Scripts/ObjectToCatch.cs
@@ -1,11 +1,20 @@
 using System;
+using DG.Tweening;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ObjectToCatch : MonoBehaviour
 {
     public static Action OnObjectCatch;
     public static Action OnObjectLost;
     
+    private void Start()
+    {
+        transform.DORotate(new Vector3(360.0f, 360.0f, 360.0f), 5.0f, RotateMode.FastBeyond360)
+            .SetLoops(-1, LoopType.Restart)
+            .SetRelative()
+            .SetEase(Ease.Linear);
+    }
     
     
     public void OnTriggerEnter(Collider other)
