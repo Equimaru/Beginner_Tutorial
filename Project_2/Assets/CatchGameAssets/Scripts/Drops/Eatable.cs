8e4d3e8dfce9103c1268357b54a98b59f9c78656
Equimaru <119777912+Equimaru@users.noreply.github.com>
2024-06-03 19:48:30 +0300

    LilCleanUp + Setting new backgrounds

--- Project_2/Assets/CatchGameAssets/Scripts/Drops/Eatable.cs
+++ Project_2/Assets/CatchGameAssets/Scripts/Drops/Eatable.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using DG.Tweening;
-using UnityEngine;
-
-namespace Catch
+﻿namespace Catch
 {
     public class Eatable : Droppable
     {
--- Project_2/Assets/CatchGameAssets/Scripts/Drops/Uneatable.cs
+++ Project_2/Assets/CatchGameAssets/Scripts/Drops/Uneatable.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using UnityEngine;
-
-namespace Catch
+﻿namespace Catch
 {
     public class Uneatable : Droppable
     {
