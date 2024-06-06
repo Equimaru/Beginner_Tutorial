0437edb764bba107ae8cced815cd38ab9c11e64b
Equimaru <119777912+Equimaru@users.noreply.github.com>
2024-06-06 09:43:09 +0300

    SelfCreating Singleton for ScoreSystem

--- Project_1/Assets/Scripts/Runner/ScoreSystem.cs
+++ Project_1/Assets/Scripts/Runner/ScoreSystem.cs
@@ -1,42 +1,61 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 namespace Runner
 {
     public class ScoreSystem : MonoBehaviour
     {
-        public static ScoreSystem Instance;
+        public static ScoreSystem Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindExistingInstances() ?? CreateNewInstance();
+                }
+
+                return _instance;
+            }
+        }
+
+        private static ScoreSystem _instance;
         
         public Action OnRecordBreak;
 
         private UIManager _uIManager;
 
-        private bool _recordIsBroken = false;
+        private bool _recordIsBroken;
         public int currentRecord;
         
         private int _score;
         
+        private static ScoreSystem CreateNewInstance()
+        {
+            var gameObject = new GameObject("ScoreSystem");
+            return gameObject.AddComponent<ScoreSystem>();
+        }
 
+        private static ScoreSystem FindExistingInstances()
+        {
+            ScoreSystem[] existingInstances = FindObjectsOfType<ScoreSystem>();
+
+            if (existingInstances == null || existingInstances.Length == 0) return null;
+
+            return existingInstances[0];
+        }
+        
         private void Awake()
         {
             DontDestroyOnLoad(gameObject);
-            
-            if (Instance == null)
-            {
-                Instance = this;
-            }
-            else if (Instance != this)
-            {
-                Destroy(gameObject);
-            }
             
             if (PlayerPrefs.HasKey("maxScore"))
             {
                 currentRecord = PlayerPrefs.GetInt("maxScore");
             }
         }
-        
+
         public void Init(UIManager uIManager)
         {
             _uIManager = uIManager;
